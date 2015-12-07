15095a82df51644a78c23810bddd36be8a5b4215
Christopher Joy-Webb <chris@joywebb.com>
2015-12-06 22:56:48 -0800

    remove Ticket open attribute - redundant because of paranoid

--- db/schema.rb
+++ db/schema.rb
@@ -183,7 +183,6 @@ ActiveRecord::Schema.define(version: 20160107182050) do
     t.string   "student_names"
     t.text     "note"
     t.string   "location"
-    t.boolean  "open",          default: true
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
